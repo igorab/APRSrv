4093aae8ab6b7adccbaea88f0855c844d8233ab2
Balakirev Igor <igor.balakirev@mail.ru>
2025-06-23 18:07:50 +0300

    robot srv

---
+++ Models/RobotTask.cs
@@ -0,0 +1,13 @@
+﻿namespace APRSrv.Models
+{
+    public class RobotTask
+    {
+        public DateTime Date { get; set; }
+
+        public int TaskId { get; set; }
+
+        public int TaskValue { get; set; }
+
+        public string? TaskName { get; set; }
+    }
+}
---
+++ Services/RobotTaskService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using APRSrv.Models;
+
+namespace APRSrv.Services
+{
+    /// <summary>
+    /// сервис для взаимодействия с API
+    /// </summary>
+    public class RobotTaskService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RobotTaskService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<RobotTask>> GetRobotTasksAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<RobotTask>>("http://localhost:5143/api/RobotTask");
+        }
+
+        public async Task<RobotTask> CreateRobotTaskAsync(RobotTask robotTask)
+        {
+            var response = await _httpClient.PostAsJsonAsync("http://localhost:5143/api/RobotTask", robotTask);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<RobotTask>();
+        }
+    }
+}
